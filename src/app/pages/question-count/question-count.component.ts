a0b831f7538bc6148b84ff630d791f6421b2f3ba
ipekyilmaz35 <ipekyi35@gmail.com>
2024-03-18 20:31:54 +0100

    2nd commit to upload files

---
+++ src/app/pages/question-count/question-count.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-question-count',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './question-count.component.html',
+  styleUrl: './question-count.component.css'
+})
+export class QuestionCountComponent implements OnChanges {
+  @Input() languageCountList: any [] = [];
+
+  loader: boolean = true;
+   
+  ngOnChanges(changes: SimpleChanges): void {
+    setTimeout(() => {
+      this.loader = false;
+    }, 2000);
+  }
+}
